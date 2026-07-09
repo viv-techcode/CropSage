68e3e38cfb3ce71a90af4cd823e908c197c167ba
Vivek Singh <vivek13122004@gmail.com>
2026-07-09 22:16:27 +0530

    Add routes for Authorization

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
