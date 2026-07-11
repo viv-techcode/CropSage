33b949b40376593797463c8dc1f39b020618e1ff
Vivek Singh <vivek13122004@gmail.com>
2026-07-11 14:03:32 +0530

    configures CORS

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,27 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
-const session = require("express-session");
-const passport = require("passport");
-
 const connectDB = require("./config/db");
-const cropRoutes = require("./routes/cropRoutes");
-const authRoutes = require("./routes/authRoutes");
-
-require("./config/passport");
 
 const app = express();
-
 connectDB();
 
-app.use(cors());
-app.use(express.json());
-
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "secret",
-    resave: false,
-    saveUninitialized: false,
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(express.json());
 
-app.use("/api/crops", cropRoutes);
+const authRoutes = require("./routes/authRoutes");
+const cropRoutes = require("./routes/cropRoutes");
+
 app.use("/api/auth", authRoutes);
+app.use("/api/crops", cropRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
